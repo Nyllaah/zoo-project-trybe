d55c66cf71e129e794c1448ca0ee8006feb9ebe8
Nyllaah <nylla.g@hotmail.com>
2023-07-17 16:11:09 -0300

    feat: req 2 done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,12 +3,4 @@ const data = require('../data/zoo_data');
 const getSpeciesByIds = (...ids) => ids.map((id) =>
   data.species.find((species) => species.id === id));
 
-// const getSpeciesByIds = (...ids) => {
-//   ids.map((id) => {
-//     data.species.find((species) => {
-//       species.id === id;
-//     });
-//   });
-// };
-
 module.exports = getSpeciesByIds;
