b4cb3870360c67215f66fc7ac20c163d7c857047
Nyllaah <nylla.g@hotmail.com>
2023-07-17 19:58:02 -0300

    feat: req 4 done

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const { employees } = data;
+
+const isManager = (id) => employees.map(({ managers }) => managers).flat()
+  .some((manager) => manager === id);
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  let subs = [];
+  if (isManager(managerId)) {
+    employees.filter((employee) => {
+      const { firstName, lastName, managers } = employee;
+      if (managers.includes(managerId)) {
+        subs.push(`${firstName} ${lastName}`);
+      } return subs;
+    });
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } return subs;
 };
+
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = { isManager, getRelatedEmployees };
