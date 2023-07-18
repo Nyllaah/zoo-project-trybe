c4b4c2ef919cab5c59c925b0a898eebd2a78a321
Nyllaah <nylla.g@hotmail.com>
2023-07-18 16:27:13 -0300

    feat: req 6 done

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,23 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('ao receber o argumento count, retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('ao receber o argumento names, retorna um array com a relação dos nomes de todos os elefantes', () => {
+    const elephants = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(elephants);
+  });
+  it('ao receber o argumento averageAge, retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  it('ao receber o argumento location, retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('ao receber o argumento popularity, retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('ao receber o argumento availability, retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
