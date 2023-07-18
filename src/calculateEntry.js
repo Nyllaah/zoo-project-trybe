decfdc9f371fa8cf1790dcb94019d23b307393bd
Nyllaah <nylla.g@hotmail.com>
2023-07-18 17:16:42 -0300

    feat: req 8 done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { adult: priceA, senior: priceS, child: priceC } = data.prices;
+
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return parseFloat(((child * priceC) + (adult * priceA) + (senior * priceS)).toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
