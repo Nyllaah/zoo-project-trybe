c529fa491dac1ce8f23ac488e0bf74df3aaa8dc9
Nyllaah <nylla.g@hotmail.com>
2023-07-18 15:18:38 -0300

    feat: req 5 done

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,15 +3,20 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 const countAnimals = (animal) => {
-  const amount = {};
   if (animal === undefined) {
+    const amount = {};
     species.forEach(({ name, residents }) => {
       amount[name] = residents.length;
     });
     return amount;
   }
+  if (animal.sex === undefined) {
+    return species.find(({ name }) => name === animal.species).residents.length;
+  }
+  return species.find(({ name }) => name === animal.species).residents
+    .filter(({ sex }) => sex === animal.sex).length;
 };
 
-countAnimals();
+countAnimals({ species: 'giraffes', sex: 'female' });
 
 module.exports = countAnimals;
