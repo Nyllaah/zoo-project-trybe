c80a79531ed8871dc464b448e18bf034c19fc9cf
Nyllaah <nylla.g@hotmail.com>
2023-07-19 17:17:51 -0300

    feat: req 9 done

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,29 +2,16 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 const { hours } = data;
-console.log(species[0].name);
 
-// fazer um objeto com os dias da semana
-// // aproveitar o objeto hours
-// // substituir o valor com as duas keys
-// // cada dia da semana tem que ter duas keys: officeHour, exhibiton
+const isAnimal = (scheduleTarget) => species.map(({ name }) => name).some((el) =>
+  el === scheduleTarget);
 
-// officehour tem que conter a frase 'Open from 8am until 6pm'
-// // pegar os horarios e colocar na frase
+const isDay = (scheduleTarget) => Object.keys(hours).some((el) => el === scheduleTarget);
 
-// exhibition tem que conter os animais que estao disponiveis nesse dia
-const animalOrDay = (scheduleTarget) => {
-  const days = Object.keys(hours);
-  const animals = Object.values(species[0].name);
-  console.log(animals);
-};
-
-animalOrDay('lion');
-
-const getAnimals = (day) => {
+const getAnimals = (scheduleTarget) => {
   const animals = [];
   species.forEach(({ name, availability }) => {
-    if (availability.includes(day)) {
+    if (availability.includes(scheduleTarget)) {
       animals.push(name);
     }
   });
@@ -38,28 +25,33 @@ const itsOpen = (day) => {
   return true;
 };
 
-const createSchedule = () => Object.keys(hours).forEach((day) => {
-  if (!itsOpen(day)) {
-    hours[day] = {
-      officeHour: 'CLOSED',
-      exhibition: 'The zoo will be closed!',
-    };
-  } else {
-    hours[day] = {
-      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
-      exhibition: getAnimals(day),
-    };
-  }
-});
-
-
-const getSchedule = (scheduleTarget) => {
-  if (scheduleTarget === undefined) {
-    return createSchedule();
-  }
-  return species.find(({ name }) => name === scheduleTarget).availability;
+const createSchedule = () => {
+  const fullSchedule = {};
+  Object.keys(hours).forEach((day) => {
+    if (!itsOpen(day)) {
+      fullSchedule[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      fullSchedule[day] = {
+        officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+        exhibition: getAnimals(day),
+      };
+    }
+  });
+  return fullSchedule;
 };
 
-getSchedule();
+const getSchedule = (scheduleTarget) => {
+  const fullSchedule = createSchedule();
+  if (isDay(scheduleTarget)) {
+    return { [scheduleTarget]: fullSchedule[scheduleTarget] };
+  }
+  if (isAnimal(scheduleTarget)) {
+    return species.find(({ name }) => name === scheduleTarget).availability;
+  }
+  return fullSchedule;
+};
 
 module.exports = getSchedule;
