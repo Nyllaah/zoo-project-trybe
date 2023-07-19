69f963ec93c3b22ae19b7fd89e848cd0e1d3843b
Nyllaah <nylla.g@hotmail.com>
2023-07-19 20:51:11 -0300

    feat: req 11 done

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { species, employees } = data;
+
+const getAnimalsNames = (ids) => {
+  const animalsNames = [];
+  ids.forEach((id) => animalsNames.push(species.find((el) => el.id === id).name));
+  return animalsNames;
+};
+
+const getLocations = (ids) => {
+  const locations = [];
+  ids.forEach((id) => locations.push(species.find((el) => el.id === id).location));
+  return locations;
+};
+
+const createFullCoverage = () => {
+  const fullCoverage = [];
+  employees.forEach((employee) => {
+    fullCoverage.push({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: getAnimalsNames(employee.responsibleFor),
+      locations: getLocations(employee.responsibleFor),
+    });
+  });
+  return fullCoverage;
+};
+
+const isEmployee = (list, info) => {
+  return list.some(({ fullName, id }) => fullName.includes(info[0]) || id === info[0]);
+};
+
+const getEmployeesCoverage = (info) => {
+  const fullCoverage = createFullCoverage();
+  if (info === undefined) {
+    return fullCoverage;
+  }
+  if (!isEmployee(fullCoverage, Object.values(info))) {
+    throw new Error('Informações inválidas');
+  }
+  return fullCoverage.find(({ id, fullName }) => fullName.includes(info.name) || id === info.id);
 };
 
 module.exports = getEmployeesCoverage;
