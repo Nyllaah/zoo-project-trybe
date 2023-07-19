9d0b2eaf4f2462cc393e8ccb553b75c721d0c8d6
Nyllaah <nylla.g@hotmail.com>
2023-07-19 19:07:25 -0300

    feat: req 10 done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const animalId = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const { residents } = species.find((el) => el.id === animalId);
+  const oldestAnimal = residents.reduce((older, curr) =>
+    ((older.age > curr.age) ? older : curr), residents[0]);
+  return Object.values(oldestAnimal);
 };
 
 module.exports = getOldestFromFirstSpecies;
