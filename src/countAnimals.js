2f046a54bfd5c559041890a068ccbafb31ae97f0
Nyllaah <nylla.g@hotmail.com>
2023-07-20 15:51:36 -0300

    refact: countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,14 +3,14 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 const countAnimals = (animal) => {
-  if (animal === undefined) {
+  if (!animal) {
     const amount = {};
     species.forEach(({ name, residents }) => {
       amount[name] = residents.length;
     });
     return amount;
   }
-  if (animal.sex === undefined) {
+  if (!animal.sex) {
     return species.find(({ name }) => name === animal.species).residents.length;
   }
   return species.find(({ name }) => name === animal.species).residents
