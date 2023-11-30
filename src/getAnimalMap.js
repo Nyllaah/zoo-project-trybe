5b015c523e7b08ca4fba221d3729ba742e70ffee
Nyllaah <nylla.g@hotmail.com>
2023-11-30 12:33:52 -0300

    map

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -10,39 +10,52 @@ const createObjBase = () => {
   return objBase;
 };
 
-const includeAnimals = (objBase) => {
-  species.forEach(({ name, location }) => {
-    objBase[location].push(name);
-  });
+// const includeAnimals = (objBase) => {
+//   species.forEach(({ name, location }) => objBase[location].push(name));
+// };
+
+const includeAnimals = (loc) => species.filter(({ location }) => location === loc)
+  .map(({ name }) => name);
+
+// const includeNames = (obj, sorted, sex) => {
+//   species.forEach(({ name, location, residents }) => {
+//     const animalResidents = { [name]: [] };
+//     if (sex) {
+//       residents.filter(({ sex: sex1 }) => sex === sex1)
+//         .forEach(({ name: resident }) => animalResidents[name].push(resident));
+//     } else {
+//       residents.forEach(({ name: resident }) => animalResidents[name].push(resident));
+//     }
+
+//     if (sorted) {
+//       animalResidents[name].sort();
+//       obj[location].push(animalResidents);
+//     } else {
+//       obj[location].push(animalResidents);
+//     }
+//   });
+// };
+
+const includeNames = () => {
+  return
 };
 
-const includeNames = (obj, sorted, sex) => {
-  species.forEach(({ name, location, residents }) => {
-    const animalResidents = { [name]: [] };
-    if (sex) {
-      residents.filter(({ sex: sex1 }) => sex === sex1)
-        .forEach(({ name: resident }) => animalResidents[name].push(resident));
-    } else {
-      residents.forEach(({ name: resident }) => animalResidents[name].push(resident));
-    }
-
-    if (sorted) {
-      animalResidents[name].sort();
-      obj[location].push(animalResidents);
-    } else {
-      obj[location].push(animalResidents);
-    }
-  });
-};
+// const getAnimalMap = (options) => {
+//   const objBase = createObjBase();
+//   if (!options || !options.includeNames) {
+//     includeAnimals(objBase);
+//   } else {
+//     includeNames(objBase, options.sorted, options.sex);
+//   }
+//   return objBase;
+// };
 
 const getAnimalMap = (options) => {
-  const objBase = createObjBase();
-  if (!options || !options.includeNames) {
-    includeAnimals(objBase);
-  } else {
-    includeNames(objBase, options.sorted, options.sex);
-  }
-  return objBase;
+  const locations = species.map(({ location }) => location);
+  return locations.reduce((acc, curr) => {
+    acc[curr] = (!options || !options.includeNames) ? includeAnimals(curr) : includeNames();
+    return acc;
+  }, {});
 };
 
 module.exports = getAnimalMap;
